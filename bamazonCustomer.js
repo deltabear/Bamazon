8ece928944ac2555d7b6013a0f1e9e243fa79d12
max delzer <emma.delzer@gmail.com>
2019-07-08 21:25:35 -0700

    created JS file

---
+++ bamazonCustomer.js
@@ -0,0 +1,5 @@
+//node application
+//when opened, display all items for sale including id, names, prices
+// then PROMPT user 2 questions
+    //1 The first should ask them the ID of the product they would like to buy.
+    //2 The second message should ask how many units of the product they would like to buy.

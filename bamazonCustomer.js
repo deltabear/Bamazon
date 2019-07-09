096f7ba35120d795ee64770fd393d8cea78d1a9d
max delzer <emma.delzer@gmail.com>
2019-07-09 13:18:22 -0700

    Created package variables and began coding

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,30 @@
-//node application
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "greatBay_DB"
+  });
+  
 //when opened, display all items for sale including id, names, prices
 // then PROMPT user 2 questions, sequentially
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  });
     //1 The first should ask them the ID of the product they would like to buy.
     //2 The second message should ask how many units of the product they would like to buy.
 // customer places order, application CHECKS IF store has enough stock to meet customer's request

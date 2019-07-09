91d1b3c2b9fc96ab1279cdf4f4307643e0df73b1
max delzer <emma.delzer@gmail.com>
2019-07-09 15:34:54 -0700

    changed database to bamazon

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,19 +12,28 @@ var connection = mysql.createConnection({
     user: "root",
   
     // Your password
-    password: "",
-    database: "greatBay_DB"
+    password: "secret",
+    database: "bamazon"
+  });
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
   });
   
+function start() {
 //when opened, display all items for sale including id, names, prices
+    inquirer
+    .prompt([
+        /* Pass your questions in here */
+    ])
+    .then(answers => {
+        // Use user feedback for... whatever!!
+    });
+
 // then PROMPT user 2 questions, sequentially
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-  });
     //1 The first should ask them the ID of the product they would like to buy.
     //2 The second message should ask how many units of the product they would like to buy.
 // customer places order, application CHECKS IF store has enough stock to meet customer's request

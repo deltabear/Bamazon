375553710343da1cc33163a6e2608d771709ce74
max delzer <emma.delzer@gmail.com>
2019-07-08 21:34:08 -0700

    Pseudocoded JS file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,11 @@
 //node application
 //when opened, display all items for sale including id, names, prices
-// then PROMPT user 2 questions
+// then PROMPT user 2 questions, sequentially
     //1 The first should ask them the ID of the product they would like to buy.
     //2 The second message should ask how many units of the product they would like to buy.
+// customer places order, application CHECKS IF store has enough stock to meet customer's request
+    // ELSE, command line responds "Product stock cannot meet customer demands. Transaction cancelled"
+    // order is prevented from going through
+// Store has enough stock:
+  // update SQL database to reflect quantity
+  // transaction goes through, shows customer total cost of their purchase

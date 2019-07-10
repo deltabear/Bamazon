32412aeac835f3d58e2ed6d3eec108038eb7070d
max delzer <emma.delzer@gmail.com>
2019-07-10 13:15:46 -0700

    Started code for prompt in JS

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,17 +25,23 @@ connection.connect(function(err) {
   
 function start() {
 //when opened, display all items for sale including id, names, prices
-    inquirer
+
+// then PROMPT user 2 questions, sequentially
+    //1 The first should ask them the ID of the product they would like to buy.
+    //2 The second message should ask how many units of the product they would like to buy.
+  inquirer
     .prompt([
-        /* Pass your questions in here */
+      {
+        name: "purchaseID",
+        type: "input",
+        message: "What product (ID only) would you like to buy?"
+      }
     ])
     .then(answers => {
         // Use user feedback for... whatever!!
     });
 
-// then PROMPT user 2 questions, sequentially
-    //1 The first should ask them the ID of the product they would like to buy.
-    //2 The second message should ask how many units of the product they would like to buy.
+
 // customer places order, application CHECKS IF store has enough stock to meet customer's request
     // ELSE, command line responds "Product stock cannot meet customer demands. Transaction cancelled"
     // order is prevented from going through

2e7c1853c9cad6426a9aa40d3c888a98e91e28f8
max delzer <emma.delzer@gmail.com>
2019-07-11 16:52:23 -0700

    Corrected names in keys.js

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 console.log('this is loaded');
 
 exports.mysqlAuth = {
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS
 }
